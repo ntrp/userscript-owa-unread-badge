556035ff6b8fddb30279abfe0d568683635cc717
Ivan Gregurić Ortolan <ivan.g.ortolan@gmail.com>
2019-05-31 10:32:59 +0200

    Optimize function
    The badge method will only be called when the value changed (otherwise the animation will play on every run). Added comments to explain the functionality.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Display Unread Badge for OWA
 // @namespace     ntrp
-// @version       1.1
+// @version       1.2
 // @description   This will check for unread messages and put the unread count in the Outlook Web App.
 // @match         https://outlook.office.com/*
 // @require       https://cdnjs.cloudflare.com/ajax/libs/favico.js/0.3.10/favico.min.js
@@ -11,21 +11,38 @@ var favicon = new Favico({
     animation: 'popFade'
 });
 
-function update() {
-    var unread = 0;
-	try {
-	    unread = [...document.querySelectorAll('span[autoId="_n_T"]')]
-            .map(node => node.innerHTML)
-            .filter(val => val.length > 0)
-            .map(val => parseInt(val))
-            .reduce((a, b) => a + b, 0);
-	} catch(err) {}
+var unread = 0;
+var interval;
 
-    if (unread <= 9) {
-        favicon.badge(unread);
-    } else {
-        favicon.badge("+");
+function update() {
+	try {
+        // query spans containing read count for all favorite folders
+	    var currentUnread = [...document.querySelectorAll('span[autoId="_n_T"]')]
+            // get value
+            .map(node => node.innerHTML)
+            // filter empty ones
+            .filter(val => val.length > 0)
+            // transform to int
+            .map(val => parseInt(val))
+            // sum
+            .reduce((a, b) => a + b, 0);
+        // if not changed do nothing
+        if (currentUnread != unread) {
+            unread = currentUnread;
+            if (unread <= 9) {
+                favicon.badge(unread);
+            } else {
+                // show plus for more than 9 unreads
+                favicon.badge("+");
+            }
+        }
+	} catch(err) {
+        // in error log error and cancel the function interval
+        console.log(err);
+        if (interval) {
+            window.clearInterval(interval);
+        }
     }
 }
 
-window.setInterval(update, 1000);
+interval = window.setInterval(update, 1000);
